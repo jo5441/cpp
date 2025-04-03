178d3fbb5865c437d3283ba16164e801d33d342d
jo5441 <2949wh@naver.com>
2025-04-03 11:39:41 +0900

    jo

--- week_5/p248_3.cpp
+++ week_5/p248_3.cpp
@@ -2,68 +2,63 @@
 using namespace std;
 
 struct mytime {
-    int hour;
-    int minute;
-    int second;
-    
-    // 기본 생성자
+    int hour, minute, second;
+
     mytime(int h = 0, int m = 0, int s = 0) : hour(h), minute(m), second(s) {}
-    
-    // 시간을 출력하는 함수
-    void print() {
-        cout << hour << ":" << minute << ":" << second << endl;
-    }
 };
-//1
-mytime addtime(mytime t1, mytime t2) {
-    int total_seconds = (t1.hour * 3600 + t1.minute * 60 + t1.second) + (t2.hour * 3600 + t2.minute * 60 + t2.second);
+
+// 1. 값에 의한 호출 (Call by Value)
+mytime addtime_by_value(mytime t1, mytime t2) {
+    int total_seconds = (t1.hour * 3600 + t1.minute * 60 + t1.second) + 
+                        (t2.hour * 3600 + t2.minute * 60 + t2.second);
     
-    int hour = total_seconds / 3600;
-    total_seconds %= 3600;
-    int minute = total_seconds / 60;
-    int second = total_seconds % 60;
-    
-    return mytime(hour, minute, second);
+    return mytime(total_seconds / 3600, (total_seconds % 3600) / 60, total_seconds % 60);
 }
-//2
-void addtime(mytime t1, mytime t2, mytime t3) {
-    int total_seconds = (t1.hour * 3600 + t1.minute * 60 + t1.second) + (t2.hour * 3600 + t2.minute * 60 + t2.second);
-    
-    t3.hour = total_seconds / 3600;
-    total_seconds %= 3600;
-    t3.minute = total_seconds / 60;
-    t3.second = total_seconds % 60;
-}
-//3
-void addtime(mytime t1, mytime t2, mytime* pt) {
-    int total_seconds = (t1.hour * 3600 + t1.minute * 60 + t1.second) + (t2.hour * 3600 + t2.minute * 60 + t2.second);
+
+// 2. 주소에 의한 호출 (Call by Address)
+void addtime_by_address(mytime t1, mytime t2, mytime* pt) {
+    int total_seconds = (t1.hour * 3600 + t1.minute * 60 + t1.second) + 
+                        (t2.hour * 3600 + t2.minute * 60 + t2.second);
     
     pt->hour = total_seconds / 3600;
-    total_seconds %= 3600;
-    pt->minute = total_seconds / 60;
+    pt->minute = (total_seconds % 3600) / 60;
     pt->second = total_seconds % 60;
+}
+
+// 3. 참조에 의한 호출 (Call by Reference)
+void addtime_by_reference(mytime t1, mytime t2, mytime &t3) {
+    int total_seconds = (t1.hour * 3600 + t1.minute * 60 + t1.second) + 
+                        (t2.hour * 3600 + t2.minute * 60 + t2.second);
+    
+    t3.hour = total_seconds / 3600;
+    t3.minute = (total_seconds % 3600) / 60;
+    t3.second = total_seconds % 60;
+}
+
+void print_time(const mytime &t) {
+    cout << t.hour << ":" << t.minute << ":" << t.second << endl;
 }
 
 int main() {
     mytime t1(2, 45, 30);
     mytime t2(3, 15, 45);
-    
-    // 1번 방법
-    mytime result1 = addtime(t1, t2);
-    cout << "Result 1: ";
-    result1.print();
-    
-    // 2번 방법
+
+    // 1. 값에 의한 호출 (Call by Value)
+    mytime result1 = addtime_by_value(t1, t2);
+    cout << "Result by Value: ";
+    print_time(result1);
+
+    // 2. 주소에 의한 호출 (Call by Address)
     mytime result2;
-    addtime(t1, t2, result2);
-    cout << "Result 2: ";
-    result2.print();
-    
-    // 3번 방법
+    addtime_by_address(t1, t2, &result2);
+    cout << "Result by Address: ";
+    print_time(result2);
+
+    // 3. 참조에 의한 호출 (Call by Reference)
     mytime result3;
-    addtime(t1, t2, &result3);
-    cout << "Result 3: ";
-    result3.print();
-    
+    addtime_by_reference(t1, t2, result3);
+    cout << "Result by Reference: ";
+    print_time(result3);
+
     return 0;
 }
